b04ebdfebe561488ef6abcf3c5ea8ccbc1908312
WasiqB <wasbhamla2005@gmail.com>
2025-11-27 12:50:49 +0530

    feat: :sparkles: initial commit

---
+++ src/tests/assignment.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('UI scenario', async () => {
+  test('Navigate to Para bank app', ({ page }) => {
+
+  });
+  test('Create new user', ({ page }) => {
+
+  });
+  test('Login with new user', ({ page }) => {
+    
+  });
+  test('Verify global menu working', ({ page }) => {
+    
+  });
+  test('Create savings account', ({ page }) => {
+    
+  });
+  test('Validate accounts overview page', ({ page }) => {
+    
+  });
+  test('Transfer funds between accounts', ({ page }) => {
+    
+  });
+  test('Pay the bill with new accounts', ({ page }) => {
+    
+  });
+});
+
+test.describe('API scenario', async () => {
+  test('Search transaction by amount', ({ request }) => {
+
+  });
+  test('Validate details displayed', ({ request }) => {
+    
+  });
+});
